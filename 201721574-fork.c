9577a173c4e3a82e8b4be5d3b69e358cb024f725
s1721574 <iloory3@naver.com>
2019-04-25 10:50:19 +0900

    mid-term

---
+++ 201721574-fork.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+int main(void) {
+
+	pid_t pid();
+
+	return 0;
+}
+
---
+++ 201721574-sender.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <sys/stat.h>
+
+int main(void) {
+
+	int num;
+	int mkfifo
+	
+	printf("num:");
+	scanf("%d",num);
+
+	return 0;
+
+}
